59ac2cbe6d282455ac559cc0660724ff0b090fbe
Thomas Lecoq <37746451+thomas-lecoq@users.noreply.github.com>
2026-05-10 20:03:35 +0200

    Add `GRID_SIZE_INPUT_CLASS` export in config.js

--- js/config.js
+++ js/config.js
@@ -7,4 +7,4 @@ const GRID_SIZE_INPUT_CLASS = 'grid-size-input';
 // app default parameters
 const DEFAULT_GRID_SIZE = {'nRows': 16, 'nCols': 16};
 
-export {WORKSPACE_CONTAINER_CLASS, DEFAULT_GRID_SIZE};
+export {WORKSPACE_CONTAINER_CLASS, GRID_SIZE_INPUT_CLASS, DEFAULT_GRID_SIZE};
