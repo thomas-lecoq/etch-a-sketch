54fda4cfb4da7908316d2c7e8a2c2d4f3e6bbc62
Thomas Lecoq <37746451+thomas-lecoq@users.noreply.github.com>
2026-05-10 20:01:51 +0200

    Add `initGridSizeInput` function
    This function manage the user input with an event listener in order to
    resize the grid on screen.

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,32 @@
 // Menu handler
+import { GRID_SIZE_INPUT_CLASS } from "./config.js"
+import { resizeGrid } from "./grid.js";
 
 // Grid size change (event = change)
+function initGridSizeInput() {
+    const input = document.querySelector(`.${GRID_SIZE_INPUT_CLASS}`);
+
+    // listen to user pressing 'Enter' key:
+    input.addEventListener('keyup', (event) => {
+        if (event.key === 'Enter') {
+            const inputValue = event.target.value;
+            const newGridSize = parseInt(inputValue, 10);
+
+            // control input quality
+            const alertText = 'Please set a grid size between 1 and 100'; 
+            switch(true) {
+                case Number.isNaN(newGridSize):
+                    return;
+                case  (newGridSize < 1 || newGridSize > 100):
+                    alert(alertText);
+                    break;
+                default:
+                    resizeGrid(newGridSize, newGridSize);
+            };
+        }
+    });
+}
 
 // Grid reset button
+
+export { initGridSizeInput };
