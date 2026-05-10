48607a2781c0186861ac15d532d4afb9bf7b4ba5
Thomas Lecoq <37746451+thomas-lecoq@users.noreply.github.com>
2026-05-10 17:49:02 +0200

    Create `appendWorkspaceTitle` function
    This function add an h2 title to workspace-ui for clarity. it's called
    through `createWorkspaceUi` function.

--- js/grid.js
+++ js/grid.js
@@ -5,12 +5,26 @@ import { WORKSPACE_CONTAINER_CLASS } from './config.js';
 import { appendNewElement } from './utils.js';
 
 /**
+ * Create new h2 title, append it to workspace-ui
+ * @param {HTMLElement} workspaceUi - workspaceUi element
+ * @param {string} text - title name  
+ * @returns {HTMLElement} newly created h2 title element
+*/
+function appendWorkspaceTitle(workspaceUi, text) {
+    const title = appendNewElement('h2', 'workspace-title', workspaceUi);
+    title.textContent = text;
+    
+    return title;
+}
+
+/**
  * Init workspace-ui, attach it to workspace-container
  * @returns {HTMLElement} workspaceUi element
 */
 function createWorkspaceUi() {
     const workspaceContainer = document.querySelector(`.${WORKSPACE_CONTAINER_CLASS}`);
     const workspaceUi = appendNewElement('div', 'workspace-ui', workspaceContainer);
+    appendWorkspaceTitle(workspaceUi, 'Draw here !');
 
     return workspaceUi;
 }
