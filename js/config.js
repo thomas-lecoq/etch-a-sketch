ed94d86e4d23b397fc2b0975d5aeeb421fc8bf7c
Thomas Lecoq <37746451+thomas-lecoq@users.noreply.github.com>
2026-05-10 19:31:52 +0200

    Add `GRID_SIZE_INPUT_CLASS` const to config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,8 @@
 // Project const
 
 // HTML classes
-const WORKSPACE_CONTAINER_CLASS = 'workspace-container'; 
+const WORKSPACE_CONTAINER_CLASS = 'workspace-container';
+const GRID_SIZE_INPUT_CLASS = 'grid-size-input'; 
 
 // app default parameters
 const DEFAULT_GRID_SIZE = {'nRows': 16, 'nCols': 16};
